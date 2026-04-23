35b3f87f4af86ed8c8865686bb25391d396dcea1
Gastone Mostardini <gastonemostardini@gmail.com>
2026-04-23 15:15:25 +0000

    feat: cicli su tutti gli inizi di partenza

--- cavallo/main.cpp
+++ cavallo/main.cpp
@@ -26,78 +26,86 @@ int main(int argc, const char* argv[]) {
 	ChessBoard board = ChessBoard(N);
 	vector<int> archi(N * N, 0);
 
-	initializeArchi(archi, N);
-
 	int i = 0;
 	int j = 0;
 
-	print(board, archi);
+	for (int ii = 0; ii < N; ii++) {
+		for (int jj = 0; jj < N; jj++) {
+			celle.clear();
+			board.clear();
+			initializeArchi(archi, N);
+			i = ii;
+			j = jj;
 
-	cin.get();
+			//print(board, archi);
 
-	cout << "i, j = " << i << ", " << j << endl;
-	board.setCell(i, j, "♞"); 
+			//cin.get();
 
-	int ni = i;
-	int nj = j;
+			cout << "i, j = " << i << ", " << j << endl;
+			board.setCell(i, j, "♞");
 
-	// Primo aggiornamento nodi vicini
-	auto vicini = horseMovableCells(N, i, j);
-	for (const auto& v : vicini) {
-		archi[pos(N, v[0], v[1])] -= 1;
-	}
+			int ni = i;
+			int nj = j;
 
-	print(board, archi, vicini);
-
-	for (size_t a = 0; a < N * N - 1; a++) {
-
-		// attesa input utente 
-		cin.get();
-
-		std::vector<std::vector<int>> celle_libere;
-
-		for (const auto& c : vicini) {
-			if (board.getCell(c[0], c[1]) == "") {
-				celle_libere.push_back(c);
+			// Primo aggiornamento nodi vicini
+			auto vicini = horseMovableCells(N, i, j);
+			for (const auto& v : vicini) {
+				archi[pos(N, v[0], v[1])] -= 1;
 			}
+
+			//print(board, archi, vicini);
+
+			for (size_t a = 0; a < N * N - 1; a++) {
+
+				// attesa input utente 
+				//cin.get();
+
+				std::vector<std::vector<int>> celle_libere;
+
+				for (const auto& c : vicini) {
+					if (board.getCell(c[0], c[1]) == "") {
+						celle_libere.push_back(c);
+					}
+				}
+
+				if (celle_libere.empty()) {
+					cout << "vicolo cieco alla mossa " << a << "\n";
+					break;
+				}
+
+				vicini = celle_libere;
+
+				std::sort(vicini.begin(), vicini.end(),
+					[&archi, N](const std::vector<int>& c, const std::vector<int>& b) {
+						return archi[pos(N, c[0], c[1])] < archi[pos(N, b[0], b[1])];
+					});
+
+				ni = vicini[0][0];
+				nj = vicini[0][1];
+
+				// Muovi il cavallo
+				board.setCell(i, j, "♘");
+				i = ni;
+				j = nj;
+				board.setCell(i, j, "♞");
+
+				// Calcolo nuove celle vicine
+				vicini = horseMovableCells(N, i, j);
+
+				// Diminuzione del valore dei nodi;
+				// La nomeclatura è un po' strana: il valore rappresenta gli archi del nodo in poszione (i, j)
+				for (const auto& v : vicini) {
+					archi[pos(N, v[0], v[1])] -= 1;
+				}
+
+				// Print della scacchiera	
+				//print(board, archi, vicini);
+			}
+
+			board.setCell(i, j, "♘");
+			//cout << board;
 		}
-
-		if (celle_libere.empty()) {
-			cout << "vicolo cieco alla mossa " << a << "\n";
-			break;
-		}
-
-		vicini = celle_libere;
-
-		std::sort(vicini.begin(), vicini.end(),
-			[&archi, N](const std::vector<int>& c, const std::vector<int>& b) {
-				return archi[pos(N, c[0], c[1])] < archi[pos(N, b[0], b[1])];
-			});
-
-		ni = vicini[0][0];
-		nj = vicini[0][1];
-
-		// Muovi il cavallo
-		board.setCell(i, j, "♘");
-		i = ni;
-		j = nj;
-		board.setCell(i, j, "♞");
-
-		// Calcolo nuove celle vicine
-		vicini = horseMovableCells(N, i, j);
-		
-		// Diminuzione del valore dei nodi;
-		// La nomeclatura è un po' strana: il valore rappresenta gli archi del nodo in poszione (i, j)
-		for (const auto& v : vicini) {
-			archi[pos(N, v[0], v[1])] -= 1;
-		}
-
-		// Print della scacchiera	
-		print(board, archi, vicini);
 	}
-
-	board.setCell(i, j, "♘");
-	cout << board;
 
 	return 0;
 }
